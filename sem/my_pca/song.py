7bff0b9149fa015426a755a2213e5f7c8cbfe2c0
ida <idahoney@gmx.at>
2023-12-06 09:43:59 +0100

    work on sem songs

--- sem/my_pca/song.py
+++ sem/my_pca/song.py
@@ -1,9 +1,31 @@
 import numpy as np
 from sklearn.decomposition import PCA
+import matplotlib
 import matplotlib.pyplot as plt
 from csv import reader
-from matplotlib.lines import Line2D
 from main import normalize, pca, read_data
+
+
+def visualize_3d(x_data, y_data, z_data, colors, labels, file_name):
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+
+    ax.scatter3D(x_data, y_data, z_data, c=colors)
+
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+    ax.set_zlabel(labels[2])
+
+    plt.savefig('data/' + file_name + '.png')
+    plt.show()
+
+
+def plot_histogram(x_data, y_data, labels, color, ax):
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", color])
+    ax.hist2d(x_data, y_data, bins=25, cmap=cmap)
+
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
 
 
 if __name__ == '__main__':
@@ -18,14 +40,6 @@ if __name__ == '__main__':
         for row in csv_reader:
             categories.append(row[0])
 
-    data = normalize(np.matrix(data))
-    ret_data, evs, pcs = pca(data)
-
-    pca = PCA(n_components=2).fit(data)
-    ref_data = np.dot(data, pca.components_.T)
-
-    x_var, y_var = 1, 10
-
     category_to_color = dict()
     category_to_color["pop"] = "crimson"
     category_to_color["rap"] = "orchid"
@@ -34,40 +48,46 @@ if __name__ == '__main__':
     category_to_color["r&b"] = "cornflowerblue"
     category_to_color["edm"] = "yellowgreen"
 
-    # visualize
-    fig, axes = plt.subplots(nrows=3, ncols=1)
-    plt.subplots_adjust(hspace=0.8)
+    x_var, y_var, z_var = 10, 1, 6
 
-    subplot1 = axes[0]
-    subplot1.set_title("original data")
-
+    # visualize original data
     x_data = [data[idx, x_var] for idx in range(data.shape[0])]
     y_data = [data[idx, y_var] for idx in range(data.shape[0])]
+    z_data = [data[idx, z_var] for idx in range(data.shape[0])]
     colors = [category_to_color[categories[idx]] for idx in range(data.shape[0])]
-    subplot1.scatter(x_data, y_data, c=colors, s=2)
-    subplot1.set_xlabel(header[x_var])
-    subplot1.set_ylabel(header[y_var])
 
-    legend_elements = []
-    for cat, col in category_to_color.items():
-        legend_elements.append(Line2D([0], [0], marker='o', color=col, label=cat, markerfacecolor=col, markersize=2))
-    subplot1.legend(handles=legend_elements)
+    visualize_3d(x_data, y_data, z_data, colors, [header[x_var], header[y_var], header[z_var]], file_name+"_org")
 
-    subplot2 = axes[1]
-    subplot2.set_title("pca data")
-    x_data = [ret_data[idx, 0] for idx in range(data.shape[0])]
-    y_data = [ret_data[idx, 1] for idx in range(data.shape[0])]
-    subplot2.scatter(x_data, y_data, c=colors, s=2)
-    subplot2.set_xlabel("PC1")
-    subplot2.set_ylabel("PC2")
+    fig, ax = plt.subplots(2, 3)
+    l = 0
+    for category, color in category_to_color.items():
+        x_data = [data[idx, x_var] for idx in range(data.shape[0]) if categories[idx] == category]
+        y_data = [data[idx, y_var] for idx in range(data.shape[0]) if categories[idx] == category]
 
-    subplot3 = axes[2]
-    subplot3.set_title("ref pca data")
-    x_data = [ref_data[idx, 0] for idx in range(data.shape[0])]
-    y_data = [ref_data[idx, 1] for idx in range(data.shape[0])]
-    subplot3.scatter(x_data, y_data, c=colors, s=2)
-    subplot3.set_xlabel("PC1")
-    subplot3.set_ylabel("PC2")
+        plot_histogram(x_data, y_data, [header[x_var], header[y_var]], color, ax[l % 2, l % 3])
+        l += 1
 
-    plt.savefig('data/' + file_name + '.png')
+    plt.show()
+
+    # pca
+    data = normalize(np.matrix(data))
+    ret_data, evs, pcs = pca(data)
+
+    # visualize pca results
+    x_data = [ret_data[idx, 0] for idx in range(ret_data.shape[0])]
+    y_data = [ret_data[idx, 1] for idx in range(ret_data.shape[0])]
+    z_data = [ret_data[idx, 2] for idx in range(ret_data.shape[0])]
+    colors = [category_to_color[categories[idx]] for idx in range(ret_data.shape[0])]
+
+    visualize_3d(x_data, y_data, z_data, colors, ["PC 1", "PC 2", "PC 3"], file_name+"_pca")
+
+    fig, ax = plt.subplots(2, 3)
+    l = 0
+    for category, color in category_to_color.items():
+        x_data = [ret_data[idx, 0] for idx in range(ret_data.shape[0]) if categories[idx] == category]
+        y_data = [ret_data[idx, 1] for idx in range(ret_data.shape[0]) if categories[idx] == category]
+
+        plot_histogram(x_data, y_data, ["PC 1", "PC 2"], color, ax[l % 2, l % 3])
+        l += 1
+
     plt.show()
