e22b3a9fed62a9e3d91165d7712e0a2a5f2333c7
ida <idahoney@gmx.at>
2023-12-14 09:01:16 +0100

    cagd ue 5? loop subdivision

--- cagd/ue4/loop.py
+++ cagd/ue4/loop.py
@@ -10,6 +10,7 @@ from geopy.mesh import Mesh
 
 def loop(M):
     vertices_from_vertex = []
+    vertex_indices = []
     for vertex in M.vertices():
         neighbor_lst = [n.point for n in vertex.vertices()]
         neighbor_lst.append(vertex.point)
@@ -19,8 +20,10 @@ def loop(M):
         weights.append(1 - n*c)
 
         vertices_from_vertex.append(np.average(neighbor_lst, axis=0, weights=weights))
+        vertex_indices.append(vertex.index)
 
     vertices_from_edge = []
+    edge_indices = []
     for edge in M.edges():
         if edge.isboundary():
             vertices_from_edge.append(np.mean([edge.origin.point, edge.target.point], axis=0))
@@ -28,9 +31,39 @@ def loop(M):
             points = [edge.origin.point, edge.target.point, edge.next.target.point,
                       edge.pair.next.target.point]
             vertices_from_edge.append(np.average(points, axis=0, weights=[3/8, 3/8, 1/8, 1/8]))
+        edge_indices.append((edge.origin.index, edge.target.index))
+
+    triangles = []
+
+    for face in M.faces():
+        ab, bc, ca = face.halfedge, face.halfedge.next, face.halfedge.prev
+        a, b, c = ab.origin, bc.origin, ca.origin
+
+        a_idx, b_idx, c_idx = a.index, b.index, c.index
+
+        try:
+            ab_idx = edge_indices.index((a_idx, b_idx))
+        except ValueError:
+            ab_idx = edge_indices.index((b_idx, a_idx))
+        try:
+            bc_idx = edge_indices.index((b_idx, c_idx))
+        except ValueError:
+            bc_idx = edge_indices.index((c_idx, b_idx))
+        try:
+            ca_idx = edge_indices.index((c_idx, a_idx))
+        except ValueError:
+            ca_idx = edge_indices.index((a_idx, c_idx))
+
+        ab_idx += len(vertices_from_vertex)
+        bc_idx += len(vertices_from_vertex)
+        ca_idx += len(vertices_from_vertex)
+
+        triangles.append((ab_idx, bc_idx, ca_idx))
+        triangles.append((a_idx, ab_idx, ca_idx))
+        triangles.append((b_idx, bc_idx, ab_idx))
+        triangles.append((c_idx, ca_idx, bc_idx))
 
     vertices = np.concatenate((vertices_from_vertex, vertices_from_edge))
-    triangles = [] # TODO
     return Mesh(vertices, triangles)
 
 
