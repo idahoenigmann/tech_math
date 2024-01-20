9573d0d55592e285f4b1bef575a5513a402f7614
Ida Hoenigmann <idahoney@gmx.at>
2024-01-20 13:59:48 +0100

    add comments

--- sem/my_pca/eeg/pca_eeg.py
+++ sem/my_pca/eeg/pca_eeg.py
@@ -9,14 +9,17 @@ from main import read_data, pca, visualize_3d
 if __name__ == "__main__":
     matplotlib.use('TkAgg')
 
+    # read data from csv
     _, data = read_data(f"n1", delimiter=",")
     data = np.matrix(data)
 
+    # read sleep stages
     sleep_stages = np.loadtxt("data/CAPsleepdatan01.csv", delimiter=";", dtype=int, skiprows=1)
     sleep_stages = sleep_stages[5:452, 0]
     color_lst = cm.jet(np.linspace(0, 1, 5))
     colors = [color_lst[sleep_stages[i] - 1] for i in range(sleep_stages.shape[0])]
 
+    # do pca or read from file
     if os.path.isfile("data/pca_output.csv"):
         pca_output = np.loadtxt("data/pca_output.csv", delimiter=",")
     else:
@@ -25,4 +28,5 @@ if __name__ == "__main__":
         print("pca finished")
         np.savetxt("data/pca_output.csv", pca_output, delimiter=",", newline="\n")
 
+    # show output
     visualize_3d(pca_output[:, 0], pca_output[:, 1], pca_output[:, 2], colors, ["pc1", "pc2", "pc3"])
--- sem/my_pca/eeg/read_raw_eeg.py
+++ sem/my_pca/eeg/read_raw_eeg.py
@@ -7,57 +7,61 @@ import mne
 if __name__ == '__main__':
     matplotlib.use('TkAgg')
 
+    plot_output = False
+
+    # read data from edf file
     file = "data/n1.edf"
     raw = mne.io.read_raw_edf(file)
     raw_data = raw.get_data(picks=['F2-F4'], start=42001, stop=6918000, return_times=True)
 
     print(raw.info)
-    # raw_data is array of channels followed by array of times
+
     data = raw_data[0][0]
     time = raw_data[1]
-    # raw.plot()
 
     print(f"Duration: {time[-1]}-{time[0]} = {time[-1] - time[0]}s")
 
+    # loop through all 30s segments
     output_data = []
-
-    start = 0
-    end = 0
+    start, end = 0, 0
     idx = 0
-    number_samplepoints = 0
-    sample_spacing = 0
+    number_samplepoints, sample_spacing = 0, 0
     while end < len(time):
         start = end
+        # find end of next 30s segment
         end = next((idx for idx, value in enumerate(time) if value > time[start] + 30), -1)
         if end == -1:
             break
 
+        # save frequencies only once
         if idx == 0:
             number_samplepoints = end - start
             sample_spacing = 1.0 / 512.0
             frequencies = np.linspace(0.0, 1.0 / (2.0 * sample_spacing), number_samplepoints // 2)
             output_data.append(frequencies)
 
+        # do fft
         x = np.linspace(0.0, number_samplepoints * sample_spacing, number_samplepoints)
         y = data[start:end]
         fft_output = np.absolute(np.fft.fft(y))
 
         # plot figures
-        """
-        fig, axs = plt.subplots(nrows=2)
-        axs[0].plot(time[start:end], data[start:end])
-        axs[0].set_xlabel("time (s)")
-        axs[0].set_ylabel("amplitude")
+        if plot_output:
+            fig, axs = plt.subplots(nrows=2)
+            axs[0].plot(time[start:end], data[start:end])
+            axs[0].set_xlabel("time (s)")
+            axs[0].set_ylabel("amplitude")
 
-        axs[1].plot(frequencies, 2.0 / number_samplepoints * fft_output[:number_samplepoints // 2])
-        axs[1].set_xlabel("frequency (Hz)")
-        axs[1].set_ylabel("amplitude")
-        
-        plt.show()
-        """
+            axs[1].plot(frequencies, 2.0 / number_samplepoints * fft_output[:number_samplepoints // 2])
+            axs[1].set_xlabel("frequency (Hz)")
+            axs[1].set_ylabel("amplitude")
 
+            plt.show()
+
+        # save result
         output_data.append(2.0/number_samplepoints*fft_output[:number_samplepoints//2])
         idx += 1
 
+    # write output to csv file
     header = "frequency, " + ",".join([f"t{i} amplitude" for i in range(idx)])
     np.savetxt(f"data/n1.csv", np.matrix(output_data).T, delimiter=",", newline="\n", header=header)
