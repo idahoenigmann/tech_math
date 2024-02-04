d363e2d6516b05c2c98e4af63210c4611d1e9d0b
Ida Hoenigmann <idahoney@gmx.at>
2024-02-04 15:27:59 +0100

    sem: validation

--- sem/my_pca/eeg/pca_eeg.py
+++ sem/my_pca/eeg/pca_eeg.py
@@ -1,62 +1,59 @@
 import numpy as np
 import matplotlib
 from matplotlib import cm
-from main import read_data, pca, visualize_3d, visualize_histogram
+from main import read_data, pca, visualize_3d, k_nearest_neighbour
 
 
-if __name__ == "__main__":
-    matplotlib.use('TkAgg')
+def read_patient_data(number):
+    _, data_seg = read_data(f"n{number}", delimiter=",")
+    sleep_stages_seg = np.loadtxt(f"data/CAPsleepdatan{number}.csv", delimiter=";", dtype=int, skiprows=1)
 
-    input_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16]
+    length = min(data_seg.shape[0], len(sleep_stages_seg))
 
+    data_seg = data_seg[0:length, :]
+    sleep_stages_seg = sleep_stages_seg[0:length, 0]
+    return data_seg, sleep_stages_seg
+
+
+def get_data_and_stages(number_list):
     data = []
     sleep_stages = []
-    for input_number in input_numbers:
-        # read data from files
-        _, data_seg = read_data(f"n{input_number}", delimiter=",")
-        sleep_stages_seg = np.loadtxt(f"data/CAPsleepdatan{input_number}.csv", delimiter=";", dtype=int, skiprows=1)
-
-        length = min(data_seg.shape[0], len(sleep_stages_seg))
-
-        data_seg = data_seg[0:length, :]
-        sleep_stages_seg = sleep_stages_seg[0:length, 0]
+    for input_number in number_list:
+        data_seg, sleep_stages_seg = read_patient_data(input_number)
 
         data.append(np.matrix(data_seg))
         sleep_stages.append(sleep_stages_seg)
     data = np.concatenate(data)
     sleep_stages = np.concatenate(sleep_stages)
+    return data, sleep_stages
 
-    print(f"Data size: {data.shape}")
+
+if __name__ == "__main__":
+    matplotlib.use('TkAgg')
+
+    train_input_numbers = [2, 4, 5, 7, 8, 9, 10, 11, 12, 15, 16]
+    val_input_numbers = [1, 3, 6]
+
+    train_data, train_sleep_stages = get_data_and_stages(train_input_numbers)
+    val_data, val_sleep_stages = get_data_and_stages(val_input_numbers)
+
+    print(f"Train data size: {train_data.shape}")
+    print(f"Val data size: {val_data.shape}")
 
     # pca
     print("pca started")
-    pca_output, eval, evec = pca(data)
+    pca_output, eval, evec = pca(train_data)
     print("pca finished")
 
-    print(f"The first {[idx for idx in range(len(eval)) if np.sum(eval[0:idx]) / np.sum(eval) > 0.5][0]} of {len(eval)}"
-          f" axis explain 50% of the variance.")
-    print(f"The first {[idx for idx in range(len(eval)) if np.sum(eval[0:idx]) / np.sum(eval) > 0.8][0]} of {len(eval)}"
-          f" axis explain 80% of the variance.")
-    print(f"The first {[idx for idx in range(len(eval)) if np.sum(eval[0:idx]) / np.sum(eval) > 0.9][0]} of {len(eval)}"
-          f" axis explain 90% of the variance.")
-    print(f"The first {[idx for idx in range(len(eval)) if np.sum(eval[0:idx]) / np.sum(eval) > 0.95][0]} of {len(eval)}"
-          f" axis explain 95% of the variance.")
+    percentages = [0.5, 0.8, 0.9, 0.95]
 
-    number_samplepoints = 30 * 200
-    sample_spacing = 1.0 / 200.0
-    frequencies = np.fft.fftfreq(number_samplepoints, d=sample_spacing)
-    frequencies = frequencies[0:len(frequencies) // 2]
-
-    # pc_analysis = np.matrix([frequencies, np.sum(evec[:26, :], axis=0).T])
-    pc_analysis = np.zeros((2, 3000))
-    pc_analysis[0, :] = frequencies
-    pc_analysis[1, :] = [e for e in np.sum(np.abs(evec[:26, :]), axis=0)][0]
-
-    np.savetxt("pc_analysis.csv", pc_analysis.T, delimiter=",", newline="\n")
+    for per in percentages:
+        print(f"The first {[idx for idx in range(len(eval)) if np.sum(eval[0:idx]) / np.sum(eval) > per][0]}"
+              f" of {len(eval)} axis explain {per * 100}% of the variance.")
 
     # generate color list
     color_lst = ["blue", "green", "yellow", "orange", "red"]
-    colors = [color_lst[sleep_stages[i] - 1] for i in range(sleep_stages.shape[0])]
+    colors = [color_lst[train_sleep_stages[i] - 1] for i in range(train_sleep_stages.shape[0])]
 
     x_data = np.squeeze(np.asarray(pca_output[:, 0]))
     y_data = np.squeeze(np.asarray(pca_output[:, 1]))
@@ -65,4 +62,24 @@ if __name__ == "__main__":
     # show output
     visualize_3d(x_data, y_data, z_data, colors, ["pc1", "pc2", "pc3"])
 
-    # visualize_histogram([x_data, y_data], ["pc1", "pc2"], colors, color_lst, 0, 1)
+    # validation
+    transformed_val = np.dot(val_data, evec)
+
+    pca_output = pca_output[:, 0:26]
+    transformed_val = transformed_val[:, 0:26]
+
+    print(["S3", "S2", "S1", "REM", "awake"])
+    for k in [1, 3, 5, 10, 15]:
+        print(f"k = {k}")
+        val_matrix = np.zeros((5, 5))
+
+        for idx in range(len(transformed_val)):
+            guess = k_nearest_neighbour(k, pca_output, train_sleep_stages, transformed_val[idx])
+            truth = val_sleep_stages[idx]
+            # print(f"guess: {guess}, truth: {truth}")
+            val_matrix[int(guess - 1), int(truth - 1)] += 1
+
+        for row in val_matrix:
+            for e in row:
+                print(int(e), end=", ")
+            print("")
