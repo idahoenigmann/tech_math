7544a4db5877893934a6275ffccf67193fce62cf
Ida Hoenigmann <idahoney@gmx.at>
2024-03-25 14:00:37 +0100

    num lower triangle system solver

--- num/algorithms/solve_triangle_system.py
+++ num/algorithms/solve_triangle_system.py
@@ -12,13 +12,33 @@ def solve_upper_triangle_system(matrix_A, vector_b):
     return vector_x
 
 
+def solve_lower_triangle_system(matrix_A, vector_b):
+    dim = len(vector_b)
+    if matrix_A.shape != (dim, dim):
+        raise ValueError("Dimension of matrix does not match dimension of vector.")
+
+    vector_x = np.zeros(dim)
+    for j in range(0, dim):
+        vector_x[j] = (vector_b[j] - np.sum([matrix_A[j, k] * vector_x[k] for k in range(0, j)])) / matrix_A[j, j]
+    return vector_x
+
+
 if __name__ == "__main__":
-    matrix_A_1 = np.matrix([[3, 1, 6],
-                          [0, 3, 4],
-                          [0, 0, 2]])
-    vector_b_1 = np.array([1, 0, 3])
+    matrix_A = np.matrix([[3, 1, 6], [0, 3, 4], [0, 0, 2]])
+    vector_b = np.array([1, 0, 3])
+    vector_x = solve_upper_triangle_system(matrix_A, vector_b)
 
-    vector_x_1 = solve_upper_triangle_system(matrix_A_1, vector_b_1)
+    print(f"x = {vector_x}, Ax = {matrix_A.dot(vector_x)}, b = {vector_b}")
 
-    print(f"x = {vector_x_1}, Ax = {matrix_A_1.dot(vector_x_1)}, b = {vector_b_1}")
+    matrix_A = np.matrix([[-5, 0, 2], [0, 4, -4], [0, 0, 1]])
+    vector_b = np.array([8, -5, 3])
+    vector_x = solve_upper_triangle_system(matrix_A, vector_b)
+
+    print(f"x = {vector_x}, Ax = {matrix_A.dot(vector_x)}, b = {vector_b}")
+
+    matrix_A = np.matrix([[-5, 0, 0], [1, 4, 0], [-7, 6, -2]])
+    vector_b = np.array([-9, 3, -8])
+    vector_x = solve_lower_triangle_system(matrix_A, vector_b)
+
+    print(f"x = {vector_x}, Ax = {matrix_A.dot(vector_x)}, b = {vector_b}")
 
