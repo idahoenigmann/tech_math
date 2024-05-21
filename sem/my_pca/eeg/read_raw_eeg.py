5e6c016bfd36812f8be369e4a74896f8c81cf3d6
Ida Hoenigmann <idahoney@gmx.at>
2024-05-21 20:29:29 +0200

    add remarks to read_raw_eeg.py

--- sem/my_pca/eeg/read_raw_eeg.py
+++ sem/my_pca/eeg/read_raw_eeg.py
@@ -4,11 +4,32 @@ import matplotlib.pyplot as plt
 import mne
 
 
+"""
+Information on this file and what to do to get it to run.
+
+Install the package mne. https://mne.tools/stable/index.html
+
+Create a directory named 'data' at the same place you saved this file. Copy all files named 'n*.edf' (where * is one of 
+{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16}) and the file 'startend.csv' into the directory 'data'.
+
+Change variable named 'input_number' to the number of recording you would like to process.
+
+Set 'plot_output' to True if you want to visualize the results. One plot is 30s of the data. To run through the entire 
+recording quickly set to False.
+
+The code will read the data, apply a low pass and high pass filter, resample the data and select a channel in the 
+recording. It then partitions the recording into 30s segments and does a FFT on each segment. The result is saved in a
+file named 'n*.csv'.
+
+The resulting csv file is a matrix of dimensions (seconds in recording / 30) times 3000. Each row is a 30s segment. In 
+the columns are the amplitudes of the different frequencies, that FFT calculated.
+"""
+
 if __name__ == '__main__':
     matplotlib.use('TkAgg')
 
-    plot_output = False
-    input_number = 1
+    plot_output = False      # if True each 30s segment and its corresponding FFT is plotted
+    input_number = 16       # recording number, e.g. 1 for reading file n1.csv
 
     start, end = np.loadtxt("data/startend.csv", delimiter=",", skiprows=1)[input_number - 1, :]
     start, end = int(start) - 1, int(end)
@@ -16,8 +37,8 @@ if __name__ == '__main__':
     # read data from edf file
     file = f"data/n{input_number}.edf"
     raw = mne.io.read_raw_edf(file, preload=True)
-    raw.filter(0.5, 30)
-    raw.resample(sfreq=200, npad=0)
+    raw.filter(0.5, 30)     # low pass and high pass filter
+    raw.resample(sfreq=200, npad=0)     # resampling to common frequency
 
     print(raw.info)
 
@@ -33,6 +54,8 @@ if __name__ == '__main__':
     time = raw_data[1][start:end]
 
     print(f"Duration: {time[-1]}-{time[0]} = {time[-1] - time[0]}s")
+
+    # stop here if you do not want to perform a FFT on the data
 
     # loop through all 30s segments
     output_data = []
